a08ec99c791e8097901760e6ab8bfbcb84692e28
Hadouweb <kengo14@hotmail.fr>
2017-01-17 07:02:19 +0100

    LOl ex00

--- D06/ex00/main.cpp
+++ D06/ex00/main.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
-#include "Convert.hpp"
+#include <iomanip>
+#include <cmath>
+#include <ctgmath>
 
 int 	main(int ac, char **av) {
-	if (ac > 1) {
-		Convert c(av[1]);
-		std::cout << "<" << c.getBase() << "> output: " << std::endl;
+	if (ac == 2) {
+		double doubleValue = static_cast<double>(std::atof(av[1]));
+		float floatValue = static_cast<float>(doubleValue);
+		int intValue = static_cast<int>(doubleValue);
+		char charValue = static_cast<char>(doubleValue);
+
+		if (intValue < 0 || intValue > 127)
+			std::cout << "char: " << "impossible" << std::endl;
+		else if (isprint(charValue))
+			std::cout << "char: '" << charValue << "'" << std::endl;
+		else
+			std::cout << "char: " << "Non displayable" << std::endl;
+
+		if (doubleValue <= INT_MAX && doubleValue >= INT_MIN)
+			std::cout <<  "int: " << intValue << std::endl;
+		else
+			std::cout << "int: " << "impossible" << std::endl;
+
+		std::cout.precision(1);
+		std::cout << "float: " << std::fixed << floatValue << 'f' << std::endl;
+		std::cout << "double: " << std::fixed << doubleValue << std::endl;
 	} else {
-		std::cout << "Usage: decimal parameter (char, int, float, double)" << std::endl;
+		std::cout << "Usage: parameter 1 (char, int, float, double)" << std::endl;
 	}
+
 	return 0;
 }
